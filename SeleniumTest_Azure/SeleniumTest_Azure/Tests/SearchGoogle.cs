ba15c964772ba3b4529f2744a6f6cf89f00f0bd8
maheswarareddy.gade <maheswarareddy.gade@outlook.com>
2019-05-02 02:41:43 -0700

    Added Test cases

---
+++ SeleniumTest_Azure/SeleniumTest_Azure/Tests/SearchGoogle.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Text;
+
+namespace SeleniumTest_Azure
+{
+   [TestClass]
+   public class SearchGoogle
+    {
+
+        [TestMethod]
+        public  void TestForEos_1()
+        {
+
+            Console.WriteLine("AZURE DevOps 1");
+
+        }
+
+
+        [TestMethod]
+        public void TestForEos_2()
+        {
+
+            Console.WriteLine("AZURE DevOps 2");
+
+        }
+
+
+        [TestMethod]
+        public void TestForEos_3()
+        {
+
+            Console.WriteLine("AZURE DevOps 3");
+
+        }
+    }
+}
