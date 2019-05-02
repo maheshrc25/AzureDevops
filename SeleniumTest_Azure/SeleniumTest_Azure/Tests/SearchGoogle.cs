05758ce00fbad25c174289984adc38308fc6edec
maheswarareddy.gade <maheswarareddy.gade@outlook.com>
2019-05-02 07:10:02 -0700

    Added Fail Test

--- SeleniumTest_Azure/SeleniumTest_Azure/Tests/SearchGoogle.cs
+++ SeleniumTest_Azure/SeleniumTest_Azure/Tests/SearchGoogle.cs
@@ -16,7 +16,7 @@ namespace SeleniumTest_Azure
         {
 
             Console.WriteLine("AZURE DevOps 1");
-            Assert.Fail();
+           
         }
 
 
@@ -25,7 +25,7 @@ namespace SeleniumTest_Azure
         {
 
             Console.WriteLine("AZURE DevOps 2");
-            Assert.Fail();
+            
 
         }
 
