b7df35eb3de1a91cda4324fd8ee81353d63531b1
maheswarareddy.gade <maheswarareddy.gade@outlook.com>
2019-06-18 06:57:18 -0700

    test added

--- SeleniumTest_Azure/SeleniumTest_Azure/Tests/BrowserTests.cs
+++ SeleniumTest_Azure/SeleniumTest_Azure/Tests/BrowserTests.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using System;
+using System.Reflection;
 
 namespace Selenium.Test
 {
@@ -10,15 +11,17 @@ namespace Selenium.Test
     public class BrowserTests
     {
         private static IWebDriver _chrome;
-       
+        private static ChromeOptions chromeOptions;
 
         [SetUp]
         public static void SetUp()
         {
+            chromeOptions= new ChromeOptions();
+            chromeOptions.AddArgument("headless");
             // Get the current directory.
             string path = System.IO.Directory.GetCurrentDirectory();
-            Console.WriteLine("The current directory is {0}", path);
-            _chrome = new ChromeDriver(@path);
+           // Console.WriteLine("The current directory is {0}", path);
+            _chrome = new ChromeDriver(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),chromeOptions);
           
         }
 
@@ -27,7 +30,6 @@ namespace Selenium.Test
         {
             _chrome.Navigate().GoToUrl("https://corridor.pramati.com/cas/login#?authType=password");
             _chrome.Close();
-
             _chrome.Dispose();
         }
     }
